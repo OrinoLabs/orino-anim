0ae3fe181f81e8a6bef70ea2cec39ce93b27e2f6
Michael Bürge <mib@orino.ch>
2018-10-30 14:55:02 +0100

    Update tsickle version.

--- dist/goog/orino-anim.js
+++ dist/goog/orino-anim.js
@@ -1,6 +1,6 @@
 /**
  * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
+ * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 goog.module('orino_anim');
 var module = module || { id: 'orino-anim.ts' };
